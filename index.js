15c99ea71f3f453cfe951822400ca56d2b35025d
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-11 10:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function shout(string){
-  return string.toUpperCase();
-}
-
-function whisper(string){
-  return string.toLowerCase();
-}
+//function shout(string){
+//  return string.toUpperCase();
+//}
+//
+//function whisper(string){
+//  return string.toLowerCase();
+//}
 
 function logShout(string){
   var uppercase = string;
